f5f6d76e856ecce2657beb7efebb627b36519341
sharesth23 <sharesth444yt@gmail.com>
2026-02-13 21:48:12 +0530

    added a function in rust script for swapstate

--- crates/core/src/swap.rs
+++ crates/core/src/swap.rs
@@ -1,3 +1,15 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use thiserror::Error;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum SwapState {
+    Created, 
+    InvoiceGenerated, 
+    OnChainFunded,
+    LighteningFunded,
+    Lightening
+    Pending,
+    Completed,
+    Failed,
+}
